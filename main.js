ae96297ae94d7d004020001c803bc6193e31b07f
Princekashyap <vasukashyap08@gmail.com>
2023-02-21 18:07:06 +0530

    i code contact section

--- main.js
+++ main.js
@@ -25,3 +25,20 @@ function closeMenu() {
 function myFunction(x) {
     x.classList.toggle("change");
 }
+
+// Toptoback 
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+    if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
+        mybutton.style.display = "block";
+    }else{
+        mybutton.style.display = "none";
+    }
+}
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
