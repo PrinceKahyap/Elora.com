27f36889aa9bb34b509ce65f985cd5d27dbda197
Princekashyap <vasukashyap08@gmail.com>
2023-02-27 20:25:56 +0530

    i have code portfolio & blog section.

--- main.js
+++ main.js
@@ -29,16 +29,16 @@ function myFunction(x) {
 // Toptoback 
 let mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
-function scrollFunction(){
-    if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
+function scrollFunction() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
         mybutton.style.display = "block";
-    }else{
+    } else {
         mybutton.style.display = "none";
     }
 }
-function topFunction(){
+function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
@@ -49,18 +49,72 @@ $(".carousel").owlCarousel({
     autoplay: true,
     autoplayTimeout: 2000,
     autoplayHoverPause: true,
-    responsive:{
-     0:{
-         items: 1,
-         nav: false
-     },
-     600:{
-         items: 2,
-         nav: false
-     },
-     1000:{
-         items: 3,
-         nav: false
-     },
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: false
+        },
     }
- });
+});
+
+$(".carousal").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: false
+        },
+    }
+});
+
+//  Typedjs 
+let typed = new Typed('#typed', {
+    strings: ["Web Development", "Graphics Designer", "Photographer"],
+    typeSpeed: 200,
+    backDelay: 100,
+    loop: true
+});
+
+
+//   filter-portfolio 
+const filterContainer = document.querySelector(".gallery-filter"),
+    galleryItems = document.querySelectorAll(".gallery-item");
+
+filterContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("filter-item")) {
+        filterContainer.querySelector(".active").classList.remove("active");
+        event.target.classList.add("active");
+
+        const filterValue = event.target.getAttribute("data-filter");
+        galleryItems.forEach((item) => {
+            if (item.classList.contains(filterValue) || filterValue === 'all') {
+                item.classList.remove("hide");
+                item.classList.add("show");
+            }
+            else {
+                item.classList.remove("show");
+                item.classList.add("hide");
+            }
+        });
+    }
+})
